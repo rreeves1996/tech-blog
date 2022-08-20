1d22b7f6a82b376cae376757d6fdd2d5b3d550d0
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-20 05:48:43 -0700

    Made main handlebars layout

--- models/index.js
+++ models/index.js
@@ -3,25 +3,25 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+    foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE'
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
+    foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
+    foreignKey: 'user_id',
 })
 
 module.exports = { User, Post, Comment };
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {maxAge:600000},
+    cookie: { 
+        maxAge:600000
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -30,7 +32,6 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
