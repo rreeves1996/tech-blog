69eb3b1b6246e3f79b63a669a5934bda61014456
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-23 14:39:27 -0700

    Fixing git rebase

--- public/js/postcreate.js
+++ public/js/postcreate.js
@@ -23,20 +23,4 @@ const newPost = async (event) => {
     }
 };
 
-const deletePost = async (event) => {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`api/post/${id}`, {
-        method: 'DELETE'
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert("Couldn't delete post!")
-    }
-};
-
-
 document.querySelector('#new-post-submit').addEventListener('click', newPost);
-document.querySelector('#delete-button').addEventListener('click', deletePost);
