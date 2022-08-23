02337c61898e4eb814956ba623eb365d5dd765c7
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-22 22:27:47 -0700

    Register/user routes all work

--- public/js/login.js
+++ public/js/login.js
@@ -19,32 +19,5 @@ const loginForm = async (event) => {
         }
     }
 };
-  
-
-// Sign up form
-const signupForm = async (event) => {
-    event.preventDefault();
-
-    const username = document.querySelector('#signup-username').value.trim();
-    const password = document.querySelector('#signup-password').value.trim();
-
-    console.log(username);
-    console.log(password);
-
-    if (username && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-  
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
-};
-document.querySelector('#signup-submit').addEventListener('click', signupForm);
 
 document.querySelector('#login-submit').addEventListener('click', loginForm);
---
+++ public/js/register.js
@@ -0,0 +1,26 @@
+// Sign up form
+const signupForm = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#signup-username').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
+
+    console.log(username);
+    console.log(password);
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+  
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#signup-submit').addEventListener('click', signupForm);
