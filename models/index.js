c9b7522df82735a958f137aeb6fa324a3418dc1c
Ryan Reeves <reeves0ryan@gmail.com>
2022-08-23 20:19:42 -0700

    Changed models again..

--- models/index.js
+++ models/index.js
@@ -7,21 +7,21 @@ User.hasMany(Post, {
     onDelete: 'CASCADE'
 });
 
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
 Post.belongsTo(User, {
     foreignKey: 'user_id'
-  });
-  
-  Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
-  
-  Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-  })
+});
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKay: 'post_id',
+    onDelete: 'CASCADE'
+});
 module.exports = { User, Post, Comment };
